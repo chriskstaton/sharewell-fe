05e683cf7aedf2c00e010864fef83b6645f143b4
Chris Staton <chris.k.staton@gmail.com>
2024-07-01 17:43:12 -0500

    add feature: click outside modal to close

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 
 interface ModalProps {
 	title: string;
@@ -8,9 +8,30 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ title, content, children, onClose }) => {
+	const modalRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				modalRef.current &&
+				!modalRef.current.contains(event.target as Node)
+			) {
+				onClose();
+			}
+		};
+
+		document.addEventListener("mousedown", handleClickOutside);
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [onClose]);
+
 	return (
 		<div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center font-sans">
-			<div className="relative bg-white p-4 rounded-md w-11/12 max-w-lg">
+			<div
+				ref={modalRef}
+				className="relative bg-white p-4 rounded-md w-11/12 max-w-lg"
+			>
 				<h2 className="text-2xl mb-4">{title}</h2>
 				<button onClick={onClose} className="absolute top-4 right-4 text-black">
 					&times;
