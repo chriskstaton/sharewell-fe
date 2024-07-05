81117c6710c2511406db7d98f99fdf953f3ab4ae
Chris Staton <chris.k.staton@gmail.com>
2024-07-05 15:16:38 -0500

    remove comment from head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,6 @@ export default function RootLayout({
 		<html lang="en">
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-				{/* <link rel="preconnect" href="https://fonts.googleapis.com" /> */}
 				<link
 					href="https://fonts.googleapis.com/css?family=Poppins"
 					rel="stylesheet"
