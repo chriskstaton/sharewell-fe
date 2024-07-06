cc7e7b2f478825a988a969c55698fd1d87da95d0
Chris Staton <chris.k.staton@gmail.com>
2024-07-05 23:47:05 -0500

    add url to todo comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			{/* <html lang="en" className={poppins.className}> */}
+			{/* <html lang="en" className={poppins.className}> https://nextjs.org/docs/app/building-your-application/optimizing/fonts */}
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
