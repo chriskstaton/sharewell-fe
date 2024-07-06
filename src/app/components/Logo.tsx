8e434ff469a658394b6df7e45283919084a7cf34
Chris Staton <chris.k.staton@gmail.com>
2024-07-05 21:45:17 -0500

    replace img with nextjs Image tag

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,8 +1,8 @@
 import React from "react";
+import Image from "next/image";
 
 const Logo: React.FC = () => {
-	return <img src="/logo.svg" alt="Logo" width="248" height="224" />;
-	//TODO
+	return <Image src="/logo.svg" alt="Logo" width={248} height={224} />;
 };
 
 export default Logo;
