64eed3a2e7dcdf4f1e96fe483d941c79f3b0d1a3
Chris Staton <chris.k.staton@gmail.com>
2024-07-08 18:30:28 -0500

    first cleanup of tailwind styles in modal

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -67,63 +67,50 @@ const Modal: React.FC<ModalProps> = (props) => {
 				return (
 					<div
 						ref={modalRef}
-						className="relative flex flex-col items-center rounded-[12px] bg-whiteNeutral web:w-[640px] web:h-[auto] tablet:w-[640px] tablet:h-[auto] mobile:w-[339px] mobile:h-[auto]"
+						className="font-sans relative flex flex-col items-center rounded-[12px] bg-whiteNeutral web:w-[640px] web:h-[auto] tablet:w-[640px] tablet:h-[auto] mobile:w-[339px] mobile:h-[auto]"
 					>
 						{/* top half of modal */}
-						<div className="flex flex-col web:h-[224px] tablet:h-[224px] mobile:h-[181px] justify-center gap-[12px] self-stretch bg-purpleNeutral web:shadow-custom tablet:shadow-none mobile:shadow-none rounded-t-[12px] relative">
-							<h2 className="text-left text-whiteNeutral web:text-[32px] tablet:text-[32px] mobile:text-[18px] font-sans font-medium web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] leading-140">
+						<div className="text-left text-whiteNeutral web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] flex flex-col web:h-[224px] tablet:h-[224px] mobile:h-[181px] justify-center gap-[12px] self-stretch bg-purpleNeutral web:shadow-custom tablet:shadow-none mobile:shadow-none rounded-t-[12px] relative">
+							<h2 className="web:text-[32px] tablet:text-[32px] mobile:text-[18px] font-medium leading-140">
 								{headerTitle}
 							</h2>
-							<div className="text-whiteNeutral text-[14px] font-sans font-medium web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] leading-140">
+							<div className="text-[14px] font-medium leading-140">
 								{headerDescription}
 							</div>
-							<div className="flex flex-col web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px]">
-								<div className="text-whiteNeutral web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-sans font-medium leading-140">
+							<div className="flex flex-col">
+								<div className="web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-medium leading-140">
 									{headerDate}
 								</div>
-								<div className="text-whiteNeutral web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-sans font-medium leading-140">
+								<div className="web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-medium leading-140">
 									{headerTime}
 								</div>
 							</div>
-
-							{/* original solution, does not match figma svg crop */}
-							{/* <div className="absolute right-0 bottom-0 mobile:absolute mobile:right-[-20px] mobile:bottom-[-36px] mobile:w-[204px] mobile:h-[204px] mobile:p-[20px]">
-								<Logo />
-							</div> */}
-
 							<div className="absolute right-0 bottom-0">
 								<LogoCombined />
 							</div>
-
-							{/* attempt to crop SVG on mobile instead of viewBox svg edits */}
-							{/* <div className="mobile:relative mobile:overflow-hidden mobile:top-[10px] mobile:right-[-10px]"> */}
 						</div>
 						{/* bottom half of modal */}
-						<div className="flex justify-between items-start px-[24px] pt-[24px] pb-[0px] self-stretch">
+						<div className="flex text-left justify-between items-start px-[24px] pt-[24px] pb-[0px] self-stretch">
 							<div className="flex flex-col gap-[4px]">
-								<div className="text-left text-charcoalNeutral font-sans text-[14px] web:font-normal tablet:font-medium mobile:font-normal leading-140">
+								<div className="text-charcoalNeutral text-[14px] web:font-normal tablet:font-medium mobile:font-normal leading-140">
 									{detailTitle}
 								</div>
-								<div className="text-left text-purpleNeutral font-sans text-[16px] font-bold leading-140">
+								<div className="text-purpleNeutral text-[16px] font-bold leading-140">
 									{detailHost}
 								</div>
 							</div>
 							<CloseButton onClick={() => closeModal(modalNumber)} />
 						</div>
-						<div className="flex flex-col pt-[16px] pb-[24px] pl-[24px] pr-[24px] justify-center items-start gap-[16px] self-stretch">
-							<div className="text-left text-blackNeutral font-sans web:text-[14px] tablet:text-[14px] mobile:text-[16px] web:font-normal tablet:font-medium mobile:font-medium leading-140">
+						<div className="flex flex-col pt-[16px] pb-[24px] pl-[24px] pr-[24px] justify-center gap-[16px] self-stretch">
+							<div className="overflow-y-auto max-h-[15vh] text-left text-blackNeutral web:text-[14px] tablet:text-[14px] mobile:text-[16px] web:font-normal tablet:font-medium mobile:font-medium leading-140">
 								{detailDescription}
 							</div>
 						</div>
-						{/* vertical line */}
-						<div className="hidden web:block tablet:block absolute right-[-33px] top-[345px] bottom-0">
-							<div className="w-[97px] h-[7px] border-b-[7px] border-r-0 border-l-0 border-t-0 rounded-full border-[#E4E4E4] mobile:border-[0] rotate-[-90deg]"></div>
-						</div>
 						<div className="flex flex-col justify-center items-center gap-[10px] self-stretch px-[24px] py-[24px] border-t border-greyAshNeutral">
-							<div className="text-center text-charcoalNeutral font-sans text-[14px] web:font-medium tablet:font-normal mobile:font-medium leading-140">
+							<div className="text-center text-charcoalNeutral text-[14px] web:font-medium tablet:font-normal mobile:font-medium leading-140">
 								{buttonDescription}
 							</div>
-							<button className="flex justify-center items-center gap-[8px] web:px-[32px] web:py-[12px] tablet:px-[32px] tablet:py-[12px] mobile:px-[72px] mobile:py-[16px] w-full rounded-[12px] bg-buttonBackground text-buttonDisabled text-[16px] web:font-normal tablet:font-semibold mobile:font-medium web:uppercase tablet:uppercase mobile:normal-case web:leading-140 tablet:leading-140 mobile:leading-100 mobile:tracking-mobileButtonSpacing mobile:font-mobileButton">
+							<button className="web:px-[32px] web:py-[12px] tablet:px-[32px] tablet:py-[12px] mobile:px-[72px] mobile:py-[16px] w-full rounded-[12px] bg-buttonBackground text-buttonDisabled text-[16px] web:font-normal tablet:font-semibold mobile:font-medium web:uppercase tablet:uppercase mobile:normal-case web:leading-140 tablet:leading-140 mobile:leading-100 mobile:tracking-mobileButtonSpacing mobile:font-mobileButton">
 								{buttonTitle}
 							</button>
 						</div>
@@ -133,58 +120,50 @@ const Modal: React.FC<ModalProps> = (props) => {
 				return (
 					<div
 						ref={modalRef}
-						className="relative flex flex-col items-center rounded-[12px] bg-whiteNeutral web:w-[640px] web:h-[auto] tablet:w-[640px] tablet:h-[auto] mobile:w-[339px] mobile:h-[auto]"
+						className="font-sans relative flex flex-col items-center rounded-[12px] bg-whiteNeutral web:w-[640px] web:h-[auto] tablet:w-[640px] tablet:h-[auto] mobile:w-[339px] mobile:h-[auto]"
 					>
 						{/* top half of modal */}
-						<div className="flex flex-col web:h-[224px] tablet:h-[224px] mobile:h-[181px] justify-center gap-[12px] self-stretch bg-purpleNeutral rounded-t-[12px] relative">
-							<h2 className="text-left text-whiteNeutral web:text-[32px] tablet:text-[32px] mobile:text-[18px] font-sans font-medium web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] leading-140">
+						<div className="text-left text-whiteNeutral web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] flex flex-col web:h-[224px] tablet:h-[224px] mobile:h-[181px] justify-center gap-[12px] self-stretch bg-purpleNeutral web:shadow-custom tablet:shadow-none mobile:shadow-none rounded-t-[12px] relative">
+							<h2 className="web:text-[32px] tablet:text-[32px] mobile:text-[18px] font-medium leading-140">
 								{headerTitle}
 							</h2>
-							<div className="text-whiteNeutral text-[14px] font-sans font-medium web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] leading-140">
+							<div className="text-[14px] font-medium leading-140">
 								{headerDescription}
 							</div>
-							<div className="flex flex-col web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px]">
-								<div className="text-whiteNeutral web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-sans font-medium leading-140">
+							<div className="flex flex-col">
+								<div className="web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-medium leading-140">
 									{headerDate}
 								</div>
-								<div className="text-whiteNeutral web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-sans font-medium leading-140">
+								<div className="web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-medium leading-140">
 									{headerTime}
 								</div>
 							</div>
-							{/* <div className="absolute right-0 bottom-0 mobile:absolute mobile:right-[-20px] mobile:bottom-[-36px] mobile:w-[204px] mobile:h-[204px] mobile:p-[20px]">
-								<Logo />
-							</div> */}
-
 							<div className="absolute right-0 bottom-0">
 								<LogoCombined />
 							</div>
 						</div>
 						{/* bottom half of modal */}
-						<div className="flex justify-between items-start px-[24px] pt-[24px] pb-[0px] self-stretch">
-							<div className="flex flex-col gap-[4px]">
-								<div className="text-left text-charcoalNeutral font-sans text-[14px] web:font-medium tablet:font-medium mobile:font-medium leading-140">
+						<div className="flex text-left justify-between items-start px-[24px] pt-[24px] pb-[0px] self-stretch">
+							<div className="flex flex-col gap-[4px] leading-140">
+								<div className="text-charcoalNeutral text-[14px] web:font-medium tablet:font-medium mobile:font-medium">
 									{detailTitle}
 								</div>
-								<div className="text-left web:text-purpleNeutral tablet:text-purpleNeutral mobile:text-charcoalNeutral font-sans text-[16px] web:font-medium tablet:font-bold mobile:font-bold leading-140 ">
+								<div className="web:text-purpleNeutral tablet:text-purpleNeutral mobile:text-charcoalNeutral text-[16px] web:font-medium tablet:font-bold mobile:font-bold leading-140">
 									{detailHost}
 								</div>
 							</div>
 							<CloseButton onClick={() => closeModal(modalNumber)} />
 						</div>
 						<div className="flex flex-col pt-[16px] pb-[24px] pl-[24px] pr-[24px] justify-center items-start gap-[16px] self-stretch">
-							<div className="text-left text-blackNeutral font-sans web:text-[14px] tablet:text-[16px] mobile:text-[14px] web:font-normal tablet:font-medium mobile:font-medium leading-140">
+							<div className="overflow-y-auto max-h-[15vh] text-left text-blackNeutral web:text-[14px] tablet:text-[16px] mobile:text-[14px] web:font-normal tablet:font-medium mobile:font-medium leading-140">
 								{detailDescription}
 							</div>
 						</div>
-						{/* vertical line */}
-						<div className="hidden web:block tablet:block absolute right-[-33px] top-[345px] bottom-0">
-							<div className="w-[97px] h-[7px] border-b-[7px] border-r-0 border-l-0 border-t-0 rounded-full border-[#E4E4E4] mobile:border-[0] rotate-[-90deg]"></div>
-						</div>
 						<div className="flex flex-col justify-center items-center gap-[10px] self-stretch px-[24px] py-[24px] border-t border-greyAshNeutral">
-							<div className="text-center text-charcoalNeutral font-sans web:text-[15px] tablet:text-[14px] mobile:text-[14px] font-medium leading-140">
+							<div className="text-center text-charcoalNeutral web:text-[15px] tablet:text-[14px] mobile:text-[14px] font-medium leading-140">
 								{buttonDescription}
 							</div>
-							<button className="flex justify-center items-center gap-[8px] web:px-[32px] web:py-[12px] tablet:px-[32px] tablet:py-[12px] mobile:px-[72px] mobile:py-[16px] w-full rounded-[12px] bg-buttonBackground text-buttonDisabled text-[16px] font-medium web:leading-140 tablet:leading-140 mobile:leading-100 mobile:tracking-mobileButtonSpacing mobile:font-mobileButton">
+							<button className="web:px-[32px] web:py-[12px] tablet:px-[32px] tablet:py-[12px] mobile:px-[72px] mobile:py-[16px] w-full rounded-[12px] bg-buttonBackground text-buttonDisabled text-[16px] font-medium web:leading-140 tablet:leading-140 mobile:leading-100 mobile:tracking-mobileButtonSpacing mobile:font-mobileButton">
 								{buttonTitle}
 							</button>
 						</div>
@@ -198,55 +177,47 @@ const Modal: React.FC<ModalProps> = (props) => {
 						className="relative flex flex-col items-center rounded-[12px] bg-whiteNeutral web:w-[640px] web:h-[auto] tablet:w-[640px] tablet:h-[auto] mobile:w-[339px] mobile:h-[auto]"
 					>
 						{/* top half of modal */}
-						<div className="flex flex-col web:h-[224px] tablet:h-[224px] mobile:h-[181px] justify-center gap-[12px] self-stretch bg-purpleNeutral rounded-t-[12px] relative">
-							<h2 className="text-left text-whiteNeutral web:text-[32px] tablet:text-[32px] mobile:text-[18px] font-sans font-medium web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] leading-140">
-								{updatedProps.headerTitle}
+						<div className="text-left text-whiteNeutral web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] flex flex-col web:h-[224px] tablet:h-[224px] mobile:h-[181px] justify-center gap-[12px] self-stretch bg-purpleNeutral web:shadow-custom tablet:shadow-none mobile:shadow-none rounded-t-[12px] relative">
+							<h2 className="web:text-[32px] tablet:text-[32px] mobile:text-[18px] font-medium leading-140">
+								{headerTitle}
 							</h2>
-							<div className="text-whiteNeutral text-[14px] font-sans font-medium web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px] leading-140">
-								{updatedProps.headerDescription}
+							<div className="text-[14px] font-medium leading-140">
+								{headerDescription}
 							</div>
-							<div className="flex flex-col web:pl-[48px] tablet:pl-[48px] mobile:pl-[24px]">
-								<div className="text-whiteNeutral web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-sans font-medium leading-140">
-									{updatedProps.headerDate}
+							<div className="flex flex-col">
+								<div className="web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-medium leading-140">
+									{headerDate}
 								</div>
-								<div className="text-whiteNeutral web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-sans font-medium leading-140">
-									{updatedProps.headerTime}
+								<div className="web:text-[16px] tablet:text-[16px] mobile:text-[14px] font-medium leading-140">
+									{headerTime}
 								</div>
 							</div>
-							{/* <div className="absolute right-0 bottom-0 mobile:absolute mobile:right-[-20px] mobile:bottom-[-36px] mobile:w-[204px] mobile:h-[204px] mobile:p-[20px]">
-								<Logo />
-							</div> */}
-
 							<div className="absolute right-0 bottom-0">
 								<LogoCombined />
 							</div>
 						</div>
 						{/* bottom half of modal */}
-						<div className="flex justify-between items-start px-[24px] pt-[24px] pb-[0px] self-stretch">
+						<div className="flex text-left justify-between items-start px-[24px] pt-[24px] pb-[0px] self-stretch">
 							<div className="flex flex-col gap-[4px]">
-								<div className="text-left web:text-charcoalNeutral tablet:text-charcoalNeutral mobile:text-purpleNeutral font-sans web:uppercase tablet:normal-case mobile:normal-case web:text-[16px] tablet:text-[14px] mobile:text-[14px] web:font-normal tablet:font-normal mobile:font-medium leading-140">
+								<div className="web:text-charcoalNeutral tablet:text-charcoalNeutral mobile:text-purpleNeutral web:uppercase tablet:normal-case mobile:normal-case web:text-[16px] tablet:text-[14px] mobile:text-[14px] web:font-normal tablet:font-normal mobile:font-medium leading-140">
 									{updatedProps.detailTitle}
 								</div>
-								<div className="text-left web:text-purpleNeutral tablet:text-purpleNeutral mobile:text-charcoalNeutral font-sans text-[16px] web:font-bold tablet:font-bold mobile:font-medium leading-140">
+								<div className="web:text-purpleNeutral tablet:text-purpleNeutral mobile:text-charcoalNeutral text-[16px] web:font-bold tablet:font-bold mobile:font-medium leading-140">
 									{updatedProps.detailHost}
 								</div>
 							</div>
 							<CloseButton onClick={() => closeModal(modalNumber)} />
 						</div>
 						<div className="flex flex-col pt-[16px] pb-[24px] pl-[24px] pr-[24px] justify-center items-start gap-[16px] self-stretch">
-							<div className="text-left text-blackNeutral font-sans web:text-[14px] tablet:text-[14px] mobile:text-[16px] font-medium leading-140">
+							<div className="overflow-y-auto max-h-[15vh] text-left text-blackNeutral web:text-[14px] tablet:text-[14px] mobile:text-[16px] font-medium leading-140">
 								{updatedProps.detailDescription}
 							</div>
-						</div>
-						{/* vertical line */}
-						<div className="hidden web:block tablet:block absolute right-[-33px] top-[345px] bottom-0">
-							<div className="w-[97px] h-[7px] border-b-[7px] border-r-0 border-l-0 border-t-0 rounded-full border-[#E4E4E4] mobile:border-[0] rotate-[-90deg]"></div>
 						</div>
 						<div className="flex flex-col justify-center items-center gap-[10px] self-stretch px-[24px] py-[24px] border-t border-greyAshNeutral">
 							<div className="text-center text-charcoalNeutral font-sans text-[14px] font-medium leading-140">
 								{updatedProps.buttonDescription}
 							</div>
-							<button className="flex justify-center items-centergap-[8px] web:px-[32px] web:py-[12px] tablet:px-[32px] tablet:py-[12px] mobile:px-[72px] mobile:py-[16px] w-full rounded-[12px] bg-buttonBackground text-buttonDisabled web:text-[18px] web:font-normal tablet:font-medium mobile:font-medium web:leading-[32px] tablet:leading-140 mobile:leading-100 web:tracking-webButtonSpacing mobile:tracking-mobileButtonSpacing mobile:font-mobileButton">
+							<button className="web:px-[32px] web:py-[12px] tablet:px-[32px] tablet:py-[12px] mobile:px-[72px] mobile:py-[16px] w-full rounded-[12px] bg-buttonBackground text-buttonDisabled web:text-[18px] web:font-normal tablet:font-medium mobile:font-medium web:leading-[32px] tablet:leading-140 mobile:leading-100 web:tracking-webButtonSpacing mobile:tracking-mobileButtonSpacing mobile:font-mobileButton">
 								{updatedProps.buttonTitle}
 							</button>
 						</div>
